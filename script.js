f1c9d7566a07c863aa7899995c6efd96abd76d13
Jack Pena <penajackc@gmail.com>
2021-12-17 08:48:03 -0600

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,4 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+h2.style.fontFamily = "'Courier New', Courier, monospace";
+document.querySelector("body").appendChild(h2);
